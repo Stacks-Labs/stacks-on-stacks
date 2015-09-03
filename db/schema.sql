266fbdf91f8d48a38e55cb0b5aca6c71297e7b3c
josh-stevens <jcstevens12@outlook.com>
2015-09-03 17:01:20 -0500

    Updated schema, wrote SQL for table creation

---
+++ db/schema.sql
@@ -0,0 +1,92 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id SERIAL NOT NULL,
+  username VARCHAR(40) NOT NULL,
+  password VARCHAR(255) DEFAULT 'sogimasogimasogima',
+  fb_id VARCHAR(255),
+  fb_token VARCHAR(255),
+  CONSTRAINT pk_users PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS trips;
+
+CREATE TABLE trips (
+  id SERIAL NOT NULL,
+  dest_name VARCHAR(255) NOT NULL,
+  geocode_latitude DOUBLE PRECISION,
+  geocode_longitude DOUBLE PRECISION,
+  time_start TIMESTAMP NOT NULL,
+  time_end TIMESTAMP NOT NULL,
+  CONSTRAINT pk_trips PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS user_trips;
+
+CREATE TABLE users_trips (
+  id SERIAL NOT NULL,
+  user_id SERIAL REFERENCES users,
+  trip_id SERIAL REFERENCES trips,
+  CONSTRAINT pk_users_trips PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS activities;
+
+CREATE TABLE activities (
+  id SERIAL NOT NULL,
+  users_trips_id SERIAL NOT NULL REFERENCES users_trips,
+  activity VARCHAR(255) NOT NULL,
+  CONSTRAINT pk_activities PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS friends;
+
+CREATE TABLE friends (
+  id SERIAL NOT NULL,
+  friender_id SERIAL NOT NULL REFERENCES users,
+  friendee_id SERIAL NOT NULL REFERENCES users,
+  CONSTRAINT pk_friends PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS messages;
+
+CREATE TABLE messages (
+  id SERIAL NOT NULL,
+  sender_id SERIAL NOT NULL REFERENCES users,
+  receiver_id SERIAL NOT NULL REFERENCES users,
+  subject VARCHAR(255) NOT NULL,
+  body TEXT NOT NULL,
+  CONSTRAINT pk_messages PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS amigo_feedback;
+
+CREATE TABLE amigo_feedback (
+  id SERIAL NOT NULL,
+  author_id SERIAL NOT NULL REFERENCES users,
+  subject_id SERIAL NOT NULL REFERENCES users,
+  feedback TEXT NOT NULL,
+  CONSTRAINT pk_amigo_feedback PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS blogs;
+
+CREATE TABLE blogs (
+  id SERIAL NOT NULL,
+  author_id SERIAL NOT NULL REFERENCES users,
+  subject VARCHAR(255) NOT NULL,
+  body TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT current_timestamp,
+  CONSTRAINT pk_blogs PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS media;
+
+CREATE TABLE media (
+  id SERIAL NOT NULL,
+  blog_id SERIAL REFERENCES blogs,
+  trip_id SERIAL REFERENCES trips,
+  user_id SERIAL REFERENCES users,
+  url TEXT NOT NULL,
+  CONSTRAINT pk_media PRIMARY KEY(id)
+);
