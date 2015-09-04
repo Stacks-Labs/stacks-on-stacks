47d905c32ef87fb0f0c0393725f5a6042b1d789c
Brian Boyko <brian.boyko@gmail.com>
2015-09-04 12:20:01 -0500

    impliment search by distance and search by time

--- models/trips.js
+++ models/trips.js
@@ -1,4 +1,3 @@
-
 // THIS MUST BE ON THE SERVER FILE FOR THIS TO WORK.
 // var databasehost = process.env.HOST || 'localhost';
 // var knex = require('knex')({
@@ -18,51 +17,100 @@ module.exports for trips.js
     Adds a trip to the database. It takes the destination name (string), start and end times (both in timestamp form).
     It also automatically calculates the geocode of the latitude and longitude (with functions to be impliemented)
     All are stored in the database. 
-  // TODO! getGeocode function which takes a destination and returns the geocode from Google Maps. 
+  deleteTrip:
+    Checks to see if any other user is referencing the trip. 
 -------------------------------------*/
 
-// var haversine = function(){
-//   function haversine() {
-//        var radians = Array.prototype.map.call(arguments, function(deg) { return deg/180.0 * Math.PI; });
-//        var lat1 = radians[0], lon1 = radians[1], lat2 = radians[2], lon2 = radians[3];
-//        var R = 6372.8; // km -- the radius of the earth (CONSTANT!)
-//        var dLat = lat2 - lat1;
-//        var dLon = lon2 - lon1;
-//        var a = Math.sin(dLat / 2) * Math.sin(dLat /2) + Math.sin(dLon / 2) * Math.sin(dLon /2) * Math.cos(lat1) * Math.cos(lat2);
-//        var c = 2 * Math.asin(Math.sqrt(a));
-//        return R * c;
-// }
-// console.log(haversine(36.12, -86.67, 33.94, -118.40));
-// }
+// BE SURE TO INCLUDE:
+// <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&v=3&libraries=geometry"></script>
+
 
 
 
 module.exports = {
   addTrip: function(destination, timeStart, timeEnd) { // string, timestamp, timestamp. 
-    var geocode = getGeocode(destination); 
+    var geocode = getGeocode(destination);
     knex('trips').insert({
-              'dest_name': destination,
-              'geocode_latitude': geocode.latitude,
-              'geocode_longitude': geocode.longitude,
-              'time_start': timeStart,
-              'time_end': timeEnd,
-              });
+      'dest_name': destination,
+      'geocode_latitude': geocode.latitude,
+      'geocode_longitude': geocode.longitude,
+      'time_start': timeStart,
+      'time_end': timeEnd,
+    });
     return true;
   },
-  deleteTrip: function(trip){
-    knex('user_trips').where{'trip_id':trip}.select().
-      then(function(tripCheck){
-        if(!tripCheck.length){
-          knex('trips').where({'id':trip}).del()
-          return true;
-        }
-        else { return false; }
-      })
-    
+  deleteTrip: function(trip) {
+    knex('user_trips').where({
+      'trip_id': trip
+    }).select().
+    then(function(tripCheck) {
+      if (!tripCheck.length) {
+        knex('trips').where({
+          'id': trip
+        }).del();
+        return true;
+      } else {
+        return false;
+      }
+    });
   },
-  getTrips: function(latitude, longitude, distance, time){
 
+  // BE SURE TO INCLUDE:
+  // <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&v=3&libraries=geometry"></script>
+  searchByDistanceAndTime: function(latitude, longitude, distance, time) {
+    return knex('trips').select()
+      .then(function(trips) {
+        return trips.map(function(trip) {
+          if (google.maps.geometry.spherical.computeDistanceBetween({
+            'lat': latitude,
+            'lng': longitude
+          }, {
+            'lat': trip.geocode_latitude,
+            'lng': trip.geocode_longitude
+          }) && time >= trip.time_start && time <= trip.time_end) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+      });
+  },
+  searchByDistance: function(latitude, longitude, distance, includePastTrips) { // int, int, int, bool
+    var time;
+    return knex('trips').select()
+      .then(function(trips) {
+        if (includePastTrips) {
+          time = 0;
+        } else {
+          time = new Date();
+        }
+        return trips.map(function(trip) {
+          if (google.maps.geometry.spherical.computeDistanceBetween({
+            'lat': latitude,
+            'lng': longitude
+          }, {
+            'lat': trip.geocode_latitude,
+            'lng': trip.geocode_longitude
+          }) && time >= trip.time_start) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+      });
+  },
+
+  searchByTime: function(begin, end) { // timestamp, timestamp
+    return knex('trips').select()
+      .then(function(trips) {
+        return trips.map(function(trip) {
+          if ((begin >= trip.time_start && begin <= trip.time_end) ||
+            (end >= trip.time_start && end <= trip.time_end)) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+      });
   }
-}
-
-
+};
