05487a83c0e645eda9a3dae19552b02d997cf4cf
josh-stevens <jcstevens12@outlook.com>
2015-09-04 09:35:53 -0500

    Modified schema for MySQL

--- db/schema.sql
+++ db/schema.sql
@@ -1,92 +1,96 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-  id SERIAL NOT NULL,
+  id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
   username VARCHAR(40) NOT NULL,
   password VARCHAR(255) DEFAULT 'sogimasogimasogima',
   fb_id VARCHAR(255),
-  fb_token VARCHAR(255),
-  CONSTRAINT pk_users PRIMARY KEY(id)
+  fb_token VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS trips;
 
 CREATE TABLE trips (
-  id SERIAL NOT NULL,
+  id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
   dest_name VARCHAR(255) NOT NULL,
   geocode_latitude DOUBLE PRECISION,
   geocode_longitude DOUBLE PRECISION,
   time_start TIMESTAMP NOT NULL,
-  time_end TIMESTAMP NOT NULL,
-  CONSTRAINT pk_trips PRIMARY KEY(id)
+  time_end TIMESTAMP NOT NULL
 );
 
-DROP TABLE IF EXISTS user_trips;
+DROP TABLE IF EXISTS users_trips;
 
 CREATE TABLE users_trips (
-  id SERIAL NOT NULL,
-  user_id INTEGER REFERENCES users,
-  trip_id INTEGER REFERENCES trips,
-  CONSTRAINT pk_users_trips PRIMARY KEY(id)
+  id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  user_id INTEGER,
+  trip_id INTEGER,
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(trip_id) REFERENCES trips(id)
 );
 
 DROP TABLE IF EXISTS activities;
 
 CREATE TABLE activities (
-  id SERIAL NOT NULL,
-  users_trips_id INTEGER NOT NULL REFERENCES users_trips,
+  id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  users_trips_id INTEGER NOT NULL,
   activity VARCHAR(255) NOT NULL,
-  CONSTRAINT pk_activities PRIMARY KEY(id)
+  FOREIGN KEY (users_trips_id) REFERENCES users_trips(id)
 );
 
 DROP TABLE IF EXISTS friends;
 
 CREATE TABLE friends (
-  id SERIAL NOT NULL,
-  friender_id INTEGER NOT NULL REFERENCES users,
-  friendee_id INTEGER NOT NULL REFERENCES users,
-  CONSTRAINT pk_friends PRIMARY KEY(id)
+  id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  friender_id INTEGER NOT NULL,
+  friendee_id INTEGER NOT NULL,
+  FOREIGN KEY (friender_id) REFERENCES users(id),
+  FOREIGN KEY (friendee_id) REFERENCES users(id)
 );
 
 DROP TABLE IF EXISTS messages;
 
 CREATE TABLE messages (
-  id SERIAL NOT NULL,
-  sender_id INTEGER NOT NULL REFERENCES users,
-  receiver_id INTEGER NOT NULL REFERENCES users,
+  id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  sender_id INTEGER NOT NULL,
+  receiver_id INTEGER NOT NULL,
   subject VARCHAR(255) NOT NULL,
   body TEXT NOT NULL,
-  CONSTRAINT pk_messages PRIMARY KEY(id)
+  FOREIGN KEY (sender_id) REFERENCES users(id),
+  FOREIGN KEY (receiver_id) REFERENCES users(id)
 );
 
 DROP TABLE IF EXISTS amigo_feedback;
 
 CREATE TABLE amigo_feedback (
-  id SERIAL NOT NULL,
-  author_id INTEGER NOT NULL REFERENCES users,
-  subject_id INTEGER NOT NULL REFERENCES users,
+  id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  author_id INTEGER NOT NULL,
+  subject_id INTEGER NOT NULL,
   feedback TEXT NOT NULL,
-  CONSTRAINT pk_amigo_feedback PRIMARY KEY(id)
+  FOREIGN KEY(author_id) REFERENCES users(id),
+  FOREIGN KEY(subject_id) REFERENCES users(id)
 );
 
 DROP TABLE IF EXISTS blogs;
 
 CREATE TABLE blogs (
-  id SERIAL NOT NULL,
-  author_id INTEGER NOT NULL REFERENCES users,
+  id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  author_id INTEGER NOT NULL,
   subject VARCHAR(255) NOT NULL,
   body TEXT NOT NULL,
   created_at TIMESTAMP DEFAULT current_timestamp,
-  CONSTRAINT pk_blogs PRIMARY KEY(id)
+  FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
 DROP TABLE IF EXISTS media;
 
 CREATE TABLE media (
-  id SERIAL NOT NULL,
-  blog_id INTEGER REFERENCES blogs,
-  trip_id INTEGER REFERENCES trips,
-  user_id INTEGER REFERENCES users,
+  id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  blog_id INTEGER,
+  trip_id INTEGER,
+  user_id INTEGER,
   url TEXT NOT NULL,
-  CONSTRAINT pk_media PRIMARY KEY(id)
+  FOREIGN KEY(blog_id) REFERENCES blogs(id),
+  FOREIGN KEY(trip_id) REFERENCES trips(id),
+  FOREIGN KEY(user_id) REFERENCES users(id)
 );
