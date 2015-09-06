4e7d45c9f0737b34f7bca79143fb2a87871b1edd
Brian Boyko <brian.boyko@gmail.com>
2015-09-05 23:04:27 -0500

    correction to activities table model

--- models/activities.js
+++ models/activities.js
@@ -38,46 +38,15 @@ var _ = require('underscore');
 
 module.exports = {
   // takes a comma delimited string, splits it into an array
-  parseInterests: function(interestStr) {
-    return interestStr.split(',');
-  },
-  updateInterests: function(interestsArr, userTripId) {
-    knex('activities').where({
-        user_trips_id: userTripId
-      }).select()
-      .then(function(userTrip) {
-        var interests = _.uniq(interestsArr.concat(JSON.parse(userTrip[0]
-          .activity)));
-        acts = JSON.stringify(interests);
-        knex('activities').update({
-          'activity': acts
-        });
-      });
-  },
-  addInterests: function(interestStr, userTripId) {
-    var dbInterests = JSON.stringify(module.exports.parseInterests(
-      interestStr));
+  addInterests: function(interest, userTripId) {
     knex('activities').insert({
       'users_trips_id': userTripId,
-      'activity': dbInterests
+      'activity': interest
     });
   },
-  removeInterests: function(interestStr, userTripId) {
-    var dbInterests = JSON.stringify(module.exports.parseInterests(
-      interestStr));
-    removeArr = module.exports.parseInterests(interestStr);
+  removeInterests: function(id) {
     knex('activities').where({
-        'user_trips_id': userTripId
-      }).select()
-      .then(function(userTrip) {
-        newInterestList = dbInterests.reject(function(element) {
-          return (removeArr.indexOf(element) > -1);
-        });
-        acts = JSON.stringify(newInterestList);
-        knex('activities').update({
-          'activity': acts
-        });
-      });
+      'id': id
+    }).del();
   }
-
 };
